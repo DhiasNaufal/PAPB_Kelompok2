dfe0aed1b8cb3d2e7573950457fd3a3c466eb6ab
Dhias Muhammad Naufal <dhiasmnaufal0@mail.ugm.ac.id>
2022-09-27 01:28:12 +0700

    adding navigation

--- app/src/main/java/com/example/layoutnavigation/HomeScreen.kt
+++ app/src/main/java/com/example/layoutnavigation/HomeScreen.kt
@@ -5,13 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
 
 class HomeScreen : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view = inflater.inflate(R.layout.fragment_home_screen,container, false)
+        view.findViewById<Button>(R.id.btn_1).setOnClickListener{
+            Navigation.findNavController(view).navigate((R.id.action_homeScreen_to_secondPage2))
+        }
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_screen, container, false)
+        return view
     }
 }
