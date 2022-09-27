30604561727b015f4f1b8a00f9e5a608ed95175b
Joseph23-11 <joseph.simarmata2311@mail.ugm.ac.id>
2022-09-27 18:12:30 +0700

    first comit

---
+++ app/src/main/java/com/example/application/onboarding/ViewPagerFragment.kt
@@ -0,0 +1,42 @@
+package com.example.application.mytestapp.onboarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
+import com.example.application.R
+import com.example.application.onboarding.ViewPagerAdapter
+import com.example.application.onboarding.screens.FirstScreen
+import com.example.application.onboarding.screens.SecondScreen
+import com.example.application.onboarding.screens.ThirdScreen
+import kotlinx.android.synthetic.main.fragment_view_pager.view.*
+
+class ViewPagerFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_view_pager, container, false)
+
+        val fragmentList = arrayListOf<Fragment>(
+            FirstScreen(),
+            SecondScreen(),
+            ThirdScreen()
+        )
+
+        val adapter = ViewPagerAdapter(
+            fragmentList,
+            requireActivity().supportFragmentManager,
+            lifecycle
+        )
+
+        adapter.also { view.viewPager.adapter = it }
+
+        return view
+    }
+
+}
