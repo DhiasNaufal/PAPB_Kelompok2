f5b0c007ba2baa1f5d04c26e34739462135d5ac4
Dhias Muhammad Naufal <dhiasmnaufal0@mail.ugm.ac.id>
2022-10-02 10:31:20 +0700

    "Activity lifeCycle"

--- app/src/main/java/com/example/layoutnavigation/MainActivity.kt
+++ app/src/main/java/com/example/layoutnavigation/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.layoutnavigation
 
+import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i("MainActivity","in method onCreate")
+        Toast.makeText(applicationContext,"onCreate",Toast.LENGTH_SHORT).show()
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("MainActivity","in method onStart")
+        Toast.makeText(applicationContext,"onStart",Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("MainActivity", "in method onResume")
+        Toast.makeText(applicationContext,"onResume",Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("MainActivity","in method onPause")
+        Toast.makeText(applicationContext,"onPause",Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("MainActivity","in method onDestroy")
+        Toast.makeText(applicationContext,"onDestroy",Toast.LENGTH_SHORT).show()
+    }
+
 }
