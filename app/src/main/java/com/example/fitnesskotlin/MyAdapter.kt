b26b40437e86d739867499d7fbbbfb1fce526648
Joseph23-11 <joseph.simarmata2311@mail.ugm.ac.id>
2022-12-12 17:58:28 +0700

    add detail gym

--- app/src/main/java/com/example/fitnesskotlin/MyAdapter.kt
+++ app/src/main/java/com/example/fitnesskotlin/MyAdapter.kt
@@ -1,13 +1,21 @@
 package com.example.fitnesskotlin
 
 import android.view.LayoutInflater
+import android.view.OnReceiveContentListener
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
-class MyAdapter(private val data: ArrayList<GymData>): RecyclerView.Adapter<ViewHolder>() {
+class MyAdapter(private val data: ArrayList<GymData>,
+                private val listener: (GymData) -> Unit
+): RecyclerView.Adapter<ViewHolder>() {
+
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind(data[position])
+        holder.itemView.setOnClickListener {
+            listener(data[position])
+        }
     }
 
     override fun getItemCount(): Int {
@@ -17,4 +25,5 @@ class MyAdapter(private val data: ArrayList<GymData>): RecyclerView.Adapter<View
         val inflater = LayoutInflater.from(parent.context)
         return ViewHolder(inflater, parent)
     }
+
 }
