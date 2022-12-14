05e21da9b1bf56b4db164fc4fe0568917eb822d4
Joseph23-11 <joseph.simarmata2311@mail.ugm.ac.id>
2022-12-14 17:48:15 +0700

    add login page gym

--- app/src/main/java/com/example/fitnesskotlin/GymActivity.kt
+++ app/src/main/java/com/example/fitnesskotlin/GymActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
+import android.widget.ImageButton
 import android.widget.ImageView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
